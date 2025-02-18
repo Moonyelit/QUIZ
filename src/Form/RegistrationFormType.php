41ae19f11ef1bdc760d8ac39409d7816b4444042
moonye@live.fr <moonye@live.fr>
2025-02-18 12:26:21 +0100

    ADD : Formulaire inscription

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,11 +4,10 @@ namespace App\Form;
 
 use App\Entity\Player;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -17,19 +16,18 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+        ->add('username', TextType::class, [
+            'label' => 'Votre pseudo',
+            'attr' => [
+                'placeholder' => 'Votre pseudo ici',
+                'required' => true,
+                'data-parsley-trigger' => 'change',
+                'data-parsley-error-message' => 'Un autre joueur à déjà ce pseudo.'
+            ]
+        ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
                 'mapped' => false,
+                'label' => 'Votre mot de passe',
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
@@ -37,13 +35,11 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
+                        'minMessage' => 'Votre mot de passe de faire au minimum {{ limit }} characters',
                         'max' => 4096,
                     ]),
                 ],
-            ])
-        ;
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,6 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        'primary': '#6E433C',
-        'secondary': '#E7C298',
-        'light': '#FEFBE8',
-        'dark': '#541A25',
-        'background': '#FDFAE6',
-      },
     },
   },
   plugins: [],
