eb6bcd21b80b15be13ad885817540aa7d5b4a7d8
TonPseudoExact <fougerouse.elodie@gmail.com>
2025-02-23 22:53:55 +0100

    ADD : swiper

---
+++ assets/Javascript/swiper.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        breakpoints: {
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 15
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            }
+        },
+        loop: false,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
